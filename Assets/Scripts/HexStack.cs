13cfcbed512ed68e9bb9f75537edf0f442556e6a
furkancanc <fcc.ceylan@gmail.com>
2025-01-15 23:48:57 +0300

    A Better Hexagon Stack

---
+++ Assets/Scripts/HexStack.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexStack : MonoBehaviour
+{
+    public List<Hexagon> Hexagons { get; private set; }
+
+    public void Add(Hexagon hexagon)
+    {
+        if (Hexagons == null)
+        {
+            Hexagons = new List<Hexagon>();
+        }
+
+        Hexagons.Add(hexagon);
+    }
+
+}
