0f2d93be94c18ae02cc221569ec71867e5bea1f2
furkancanc <fcc.ceylan@gmail.com>
2025-01-15 23:43:25 +0300

    Coloring the Stacks

---
+++ Assets/Scripts/Hexagon.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Hexagon : MonoBehaviour
+{
+    [Header(" Elements ")]
+    [SerializeField] private new Renderer renderer;
+
+    public Color Color
+    {
+        get => renderer.material.color;
+        set => renderer.material.color = value;
+    }
+
+}
--- Assets/Scripts/StackSpawner.cs
+++ Assets/Scripts/StackSpawner.cs
@@ -1,11 +1,19 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class StackSpawner : MonoBehaviour
 {
     [Header(" Elements ")]
     [SerializeField] private Transform stackPositionsParent;
-    [SerializeField] private GameObject hexagonPrefab;
+    [SerializeField] private Hexagon hexagonPrefab;
     [SerializeField] private GameObject hexagonStackPrefab;
+
+    [Header(" Settings ")]
+    [NaughtyAttributes.MinMaxSlider(2, 8)]
+    [SerializeField] private Vector2Int minMaxHexCount;
+    [SerializeField] private Color[] colors;
 
     void Start()
     {
@@ -25,12 +33,43 @@ public class StackSpawner : MonoBehaviour
         GameObject hexStack = Instantiate(hexagonStackPrefab, parent.position, Quaternion.identity, parent);
         hexStack.name = $"Stack {parent.GetSiblingIndex()}";
 
-        int amount = Random.Range(2, 7);
+        int amount = Random.Range(minMaxHexCount.x, minMaxHexCount.y);
+        int firstColorHexagonCount = Random.Range(0, amount);
+
+        Color[] colorArray = GetRandomColors();
+
+
         for (int i = 0; i < amount; ++i)
         {
             Vector3 hexagonLocalPos = Vector3.up * i * .2f;
             Vector3 spawnPosition = hexStack.transform.TransformPoint(hexagonLocalPos);
-            GameObject hexagonInstance = Instantiate(hexagonPrefab, spawnPosition, Quaternion.identity, hexStack.transform);
+            Hexagon hexagonInstance = Instantiate(hexagonPrefab, spawnPosition, Quaternion.identity, hexStack.transform);
+
+            hexagonInstance.Color = i < firstColorHexagonCount ? colorArray[0] : colorArray[1];
         }
+    }
+
+    private Color[] GetRandomColors()
+    {
+        List<Color> colorList = new List<Color>();
+        colorList.AddRange(colors);
+
+        if (colorList.Count <= 0)
+        {
+            Debug.LogError("No color found!");
+            return null;
+        }
+
+        Color firstColor = colorList.OrderBy(x => Random.value).First();
+        colorList.Remove(firstColor);
+
+        if (colorList.Count <= 0)
+        {
+            Debug.LogError("Only one color was found");
+        }
+
+        Color secondColor = colorList.OrderBy(x => Random.value).First();
+
+        return new Color[] {firstColor, secondColor};
     }
 }
