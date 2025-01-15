0bb9fea627d7cd9b7a604ba260b8855f3a87ea28
furkancanc <fcc.ceylan@gmail.com>
2025-01-15 23:04:55 +0300

    Procedural Hexagonal Grid

---
+++ Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,33 @@
+using NaughtyAttributes;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+    [Header(" Elements ")]
+    [SerializeField] private Grid grid;
+    [SerializeField] private GameObject hexagon;
+
+    [Header(" Settings ")]
+    [OnValueChanged("GenerateGrid")]
+    [SerializeField] private int gridSize;
+
+    private void GenerateGrid()
+    {
+        transform.Clear();
+
+        for (int x = -gridSize; x <= gridSize; ++x)
+        {
+            for (int y = -gridSize; y <= gridSize; ++y)
+            {
+                Vector3 spawnPosition = grid.CellToWorld(new Vector3Int(x, y, 0));
+
+                if (spawnPosition.magnitude > grid.CellToWorld(new Vector3Int(1, 0, 0)).magnitude * gridSize)
+                {
+                    continue;
+                }
+
+                Instantiate(hexagon, spawnPosition, Quaternion.identity, transform);
+            }
+        }
+    }
+}
